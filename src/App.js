a3392e8cedecd34fe63122926e4ab8ade851f0b9
hakan <tasbeyhakan@gmail.com>
2023-12-04 13:00:27 +0300

    end of the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+import data from "./data";
+import List from "./List";
 
-import './App.css';
 
 function App() {
+  const[people,setPeople]=useState(data)
   return (
-
+    <main>
+    <div className="container">
+      <h3>{people.length} Person Birthdays Today</h3>
+      <List people={people}/>
+      <button onClick={()=>setPeople([])}>Clear All</button>
+    </div>
+    </main>
+  );
 }
 
 export default App;
